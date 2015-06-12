bceb2bd2c5a33a6e60f5abd9b08aa4c775c63185
Benjamin Lee <benjamsandwich@gmail.com>
2015-06-12 01:36:45 -0700

    Add example mocha test spec

---
+++ nodeapp/test/testSpec.js
@@ -0,0 +1,7 @@
+var assert = require('assert');
+
+describe('multiplying two negative numbers', function() {
+	it('should equal a positive number', function() {
+		assert.equal(-1*-1, 1);
+	});
+});
